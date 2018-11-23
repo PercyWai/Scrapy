9e20806cca1292621ff98879d83595545ba8a785
PercyWai <41949532+PercyWai@users.noreply.github.com>
2018-11-23 11:14:52 +0800

    Delete ss.py

--- name/ss.py
+++
@@ -1,7 +0,0 @@
-list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 3, 5, 7, 9]
-for i in range(0, len(list1)):
-    for j in range(i + 1, len(list1)):
-        if list1[i] == list1[j]:
-            list1.remove(list1[i])
-
-print(list1)
